9074fb0f0cd636941635fefe518421bea1856b4a
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-12-12 15:48:09 +0530

    Add files via upload

---
+++ code1/lib/screens/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Home() {}
+
+  AppBar _getAppBar() {
+    return AppBar(
+      //elevation: 10,
+      //shadowColor: Colors.purpleAccent,
+      // leading: Icon(
+      //   Icons.home,
+      //   //size: 30,
+      //   //color: Colors.black,
+      // ),
+      actions: [
+        Icon(
+          Icons.notifications,
+          //size: 22,
+        ),
+        Icon(
+          Icons.email,
+          // size: 22
+        )
+      ],
+      // backgroundColor: Colors.yellowAccent,
+      title: Text(
+        'First App',
+        style: TextStyle(
+            fontSize: 22, color: Colors.redAccent, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        elevation: 5,
+        child: Icon(Icons.email),
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+      ),
+      // backgroundColor: Colors.tealAccent,
+      appBar: _getAppBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        elevation: 4,
+        backgroundColor: Colors.purpleAccent,
+        child: Icon(
+          Icons.mic,
+          size: 30,
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(top: 100, left: 100),
+          // padding: EdgeInsets.all(100),
+          //padding: EdgeInsets.only(left: 100, top: 200),
+          // padding: EdgeInsets.only(top: 300, bottom: 100),
+          padding: EdgeInsets.symmetric(horizontal: 100),
+          width: size.width,
+          height: size.height / 2,
+          //width: double.infinity,
+          //height: double.infinity,
+          child: Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 32),
+          ),
+          color: Colors.orangeAccent,
+        ),
+        // child: Text(
+        //   'Hello Flutter',
+        //   style: TextStyle(fontSize: 22),
+        // ),
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+          elevation: 4,
+          child: Center(child: Text('My Drawer')),
+        ),
+      ),
+    );
+    // return Center(
+    //   child: Image.network(
+    //       'https://upload.wikimedia.org/wikipedia/commons/5/55/Geely_assembly_line_in_Beilun%2C_Ningbo.JPG'),
+    // );
+  }
+}
